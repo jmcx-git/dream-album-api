c4bbce7f8dbe73dedd95aff14ad35c237bae90f4
yhx <yhx@10.1.1.195>
2016-12-07 19:10:06 +0800

    用户

---
+++ dream-core/src/main/java/com/dreambox/core/service/album/UserInfoService.java
@@ -0,0 +1,8 @@
+package com.dreambox.core.service.album;
+
+import com.dreambox.core.dto.album.UserInfo;
+import com.dreambox.core.service.AbsCommonDataService;
+
+public abstract class UserInfoService extends AbsCommonDataService<UserInfo> {
+
+}
