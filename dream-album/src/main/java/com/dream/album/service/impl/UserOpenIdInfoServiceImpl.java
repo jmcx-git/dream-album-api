e027358da99fa751345dca666c11a1c3b56f44d9
luofei <luofei@appchina.com>
2016-12-27 14:37:55 +0800

    add support wechat small app developer info

--- dream-album/src/main/java/com/dream/album/service/impl/UserOpenIdInfoServiceImpl.java
+++ dream-album/src/main/java/com/dream/album/service/impl/UserOpenIdInfoServiceImpl.java
@@ -1,28 +1,52 @@
 package com.dream.album.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dream.album.model.UserOpenIdInfo;
 import com.dream.album.service.UserOpenIdInfoService;
-import com.dreambox.core.utils.WxCommanUtils;
+import com.dreambox.core.dto.album.SmallAppDeveloperInfo;
+import com.dreambox.core.service.album.SmallAppDeveloperInfoService;
+import com.dreambox.web.exception.ServiceException;
 import com.dreambox.web.utils.GsonUtils;
 import com.dreambox.web.utils.RemoteDataUtil;
 
 @Service("userOpenIdInfoService")
 public class UserOpenIdInfoServiceImpl implements UserOpenIdInfoService {
-
     public static Logger log = Logger.getLogger(UserOpenIdInfoServiceImpl.class);
+    public static String GRANT_TYPE = "authorization_code";
+    @Autowired
+    private SmallAppDeveloperInfoService smallAppDeveloperInfoService;
 
     @Override
     public UserOpenIdInfo getUser3rdSesseion(String code) {
-        String api = "https://api.weixin.qq.com/sns/jscode2session?appid=" + WxCommanUtils.appid + "&secret="
-                + WxCommanUtils.secret + "&js_code=" + code + "&grant_type=" + WxCommanUtils.grantType;
+        return getUser3rdSesseion(null, code);
+    }
+
+    @Override
+    public UserOpenIdInfo getUser3rdSesseion(String appId, String code) {
+        if (StringUtils.isEmpty(appId)) {
+            appId = "wx0ddc8673b8df3827";
+        }
+        SmallAppDeveloperInfo g = new SmallAppDeveloperInfo();
+        g.setAppId(appId);
+        g = smallAppDeveloperInfoService.getInfoByUk(g);
+        if (g == null) {
+            throw ServiceException.getInternalException("Unknown appid for wechat small app developer.");
+        }
+        return getUser3rdSesseion(code, g);
+    }
+
+    private UserOpenIdInfo getUser3rdSesseion(String code, SmallAppDeveloperInfo g) {
+        String api = "https://api.weixin.qq.com/sns/jscode2session?appid=" + g.getAppId() + "&secret=" + g.getSecret()
+                + "&js_code=" + code + "&grant_type=" + GRANT_TYPE;
         String ret = null;
         try {
-            ret = RemoteDataUtil.getRemote(api, 30000, 3);
+            ret = RemoteDataUtil.getRemote(api, 5000, 3);
         } catch (Exception e) {
-            log.error("gain user openid failed.code=" + code);
+            throw ServiceException.getInternalException("Get user session code failed. Network error.");
         }
         if (ret == null) {
             return null;
@@ -33,7 +57,7 @@ public class UserOpenIdInfoServiceImpl implements UserOpenIdInfoService {
                     return info;
                 }
             } catch (Exception e) {
-                log.error("parse opendid failed.code=" + code);
+                throw ServiceException.getInternalException("Convert user session code failed. Convert error.");
             }
         }
         return null;
