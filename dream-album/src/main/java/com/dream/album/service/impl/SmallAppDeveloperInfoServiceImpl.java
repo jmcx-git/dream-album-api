4f4cac3e87d87e287c64bf3c625f2366f7503680
luofei <luofei@appchina.com>
2016-12-27 14:38:26 +0800

    add support wechat small app developer info

---
+++ dream-album/src/main/java/com/dream/album/service/impl/SmallAppDeveloperInfoServiceImpl.java
@@ -0,0 +1,77 @@
+// Copyright 2016 https://mokous.com Inc. All Rights Reserved.
+
+package com.dream.album.service.impl;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.ShardedJedisPool;
+
+import com.dream.album.dao.SmallAppDeveloperInfoDao;
+import com.dreambox.core.dao.CommonDao;
+import com.dreambox.core.dao.LoadDao;
+import com.dreambox.core.dto.album.SmallAppDeveloperInfo;
+import com.dreambox.core.service.album.SmallAppDeveloperInfoService;
+import com.dreambox.core.utils.RedisCacheUtils;
+
+/**
+ * @author mokous86@gmail.com create date: 2016年12月27日
+ *
+ */
+@Service("smallAppDeveloperInfoService")
+public class SmallAppDeveloperInfoServiceImpl extends SmallAppDeveloperInfoService {
+    private static final Logger log = Logger.getLogger(SmallAppDeveloperInfoServiceImpl.class);
+    @Autowired
+    private SmallAppDeveloperInfoDao smallAppDeveloperInfoDao;
+    @Resource(name = "jmcx-wx-redisdbpool")
+    private JedisPool jedisDbPool;
+    @Resource(name = "jmcx-wx-rediscacheshardedpool")
+    private ShardedJedisPool shardedJedisPool;
+    private String idInfoKey = "sa:dev:info:id";
+    private String pkUkPrefixKey = "sa:dev:info:id:uk";
+
+    @Override
+    protected ShardedJedisPool getSharedJedisPool() {
+        return shardedJedisPool;
+    }
+
+    @Override
+    protected String buildDataInfoKey(int id) {
+        return RedisCacheUtils.buildKey(idInfoKey, id);
+    }
+
+    @Override
+    public CommonDao<SmallAppDeveloperInfo> getCommonDao() {
+        return smallAppDeveloperInfoDao;
+    }
+
+    @Override
+    protected String buildUkPkPrefixKey() {
+        return pkUkPrefixKey;
+    }
+
+    @Override
+    protected JedisPool getJedisPool() {
+        return jedisDbPool;
+    }
+
+    @Override
+    protected LoadDao<SmallAppDeveloperInfo> getLoadDao() {
+        return smallAppDeveloperInfoDao;
+    }
+
+    @Override
+    protected boolean isStop() {
+        return false;
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
+    }
+
+}
---
+++ dream-album/src/main/resources/db/sql/201612270001.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `smallapp_developer_info`(
+	`id` int unsigned not null auto_increment,
+	`app_id` varchar(32) not null,
+	`secret` varchar(64) not null,
+	`name` varchar(64) not null,
+	`status` int(11) NOT NULL DEFAULT '0',
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `create_time` datetime NOT NULL COMMENT '记录创建时间',
+    PRIMARY KEY (`id`),
+     UNIQUE KEY `UK_SD_APPID`(`app_id`)
+)Engine=InnoDB DEFAULT CHARSET=utf8 COMMENT '小程序开发者信息表';
+
+INSERT INTO `smallapp_developer_info` values(null, 'wxdfbe82261831dda7', 'eabaf170cf7c3993d3fbf02eadae42ac', '萌娃相册', 0, now(), now());
+INSERT INTO `smallapp_developer_info` values(null, 'wx0ddc8673b8df3827', '16619bf042c6ef47965f45af7e09afe8', '水滴相册', 0, now(), now());
