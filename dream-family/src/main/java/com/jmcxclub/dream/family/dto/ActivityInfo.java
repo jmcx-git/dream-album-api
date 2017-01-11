9368914d2717f8f14c11205f29ca3faf3bb3e03b
luofei <luofei@appchina.com>
2017-01-11 14:27:48 +0800

    fix add space bug, space feed list likes comments will return user openid

---
+++ dream-family/src/main/java/com/jmcxclub/dream/family/dto/ActivityInfo.java
@@ -0,0 +1,18 @@
+// Copyright 2017 https://mokous.com Inc. All Rights Reserved.
+
+package com.jmcxclub.dream.family.dto;
+
+import com.dreambox.core.dto.StatusSerializable;
+
+/**
+ * @author mokous86@gmail.com create date: Jan 11, 2017
+ *
+ */
+public class ActivityInfo extends StatusSerializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 22017033387361401L;
+
+}
