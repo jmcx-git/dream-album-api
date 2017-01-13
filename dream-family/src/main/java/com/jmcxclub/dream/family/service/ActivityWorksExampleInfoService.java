8696a093ec020cc4ebfa053e66bcd5339db1cd8e
luofei <luofei@appchina.com>
2017-01-13 17:35:51 +0800

    Add support for activity detail

---
+++ dream-family/src/main/java/com/jmcxclub/dream/family/service/ActivityWorksExampleInfoService.java
@@ -0,0 +1,32 @@
+// Copyright 2017 https://mokous.com Inc. All Rights Reserved.
+
+package com.jmcxclub.dream.family.service;
+
+import com.dreambox.core.cache.CacheFilter.StartSizeCacheFilter;
+import com.dreambox.core.service.AbstractSetCacheService;
+import com.jmcxclub.dream.family.dto.ActivityWorksExampleInfo;
+
+/**
+ * @author mokous86@gmail.com create date: Jan 13, 2017
+ *
+ */
+public abstract class ActivityWorksExampleInfoService extends AbstractSetCacheService<ActivityWorksExampleInfo> {
+    public static class ActivityWorksExampleInfoSetCacheFilter extends StartSizeCacheFilter {
+        private int activityId;
+
+        public ActivityWorksExampleInfoSetCacheFilter(int activityId) {
+            this.activityId = activityId;
+            super.setStart(0);
+            super.setSize(Integer.MAX_VALUE);
+        }
+
+        public int getActivityId() {
+            return activityId;
+        }
+
+        public void setActivityId(int activityId) {
+            this.activityId = activityId;
+        }
+    }
+
+}
