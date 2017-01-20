efb0cf7399f4e7c39c2cf17e5543475f26de931d
luofei <luofei@appchina.com>
2017-01-20 17:16:20 +0800

    fix josn bug

--- dream-family/src/main/java/com/jmcxclub/dream/family/model/AboutUsResp.java
+++ dream-family/src/main/java/com/jmcxclub/dream/family/model/AboutUsResp.java
@@ -6,6 +6,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.dreambox.web.utils.GsonUtils;
+
 /**
  * @author mokous86@gmail.com create date: Jan 20, 2017
  *
@@ -15,10 +17,10 @@ public class AboutUsResp implements Serializable {
      * 
      */
     private static final long serialVersionUID = -3298197307282539699L;
-    public static final String solgan = "";
-    public static final String officialServiceQrCode = "https://cdn.mokous.com/static/qrcode/official_service_qucode_170127.jpeg";// 服务群
-    public static final String mpQrCode = "https://cdn.mokous.com/static/qrcode/official_service_qucode_170127.jpeg";// 公方号
-    public static final List<String> bottomDesctions = new ArrayList<String>() {
+    private String solgan = "";
+    private String officialServiceQrCode = "https://cdn.mokous.com/static/qrcode/official_service_qucode_170127.jpeg";// 服务群
+    private String mpQrCode = "https://cdn.mokous.com/static/qrcode/official_service_qucode_170127.jpeg";// 公方号
+    private List<String> bottomDesctions = new ArrayList<String>() {
         /**
          * 
          */
@@ -28,4 +30,40 @@ public class AboutUsResp implements Serializable {
             add("官方QQ群:612883308");
         }
     };
+
+    public static void main(String[] args) {
+        System.out.println(GsonUtils.toJson(new AboutUsResp()));
+    }
+
+    public String getSolgan() {
+        return solgan;
+    }
+
+    public void setSolgan(String solgan) {
+        this.solgan = solgan;
+    }
+
+    public String getOfficialServiceQrCode() {
+        return officialServiceQrCode;
+    }
+
+    public void setOfficialServiceQrCode(String officialServiceQrCode) {
+        this.officialServiceQrCode = officialServiceQrCode;
+    }
+
+    public String getMpQrCode() {
+        return mpQrCode;
+    }
+
+    public void setMpQrCode(String mpQrCode) {
+        this.mpQrCode = mpQrCode;
+    }
+
+    public List<String> getBottomDesctions() {
+        return bottomDesctions;
+    }
+
+    public void setBottomDesctions(List<String> bottomDesctions) {
+        this.bottomDesctions = bottomDesctions;
+    }
 }
